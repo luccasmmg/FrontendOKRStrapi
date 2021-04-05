f17ff80031b5a2741ecf48f6841d7b67a3a65a71
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2021-04-04 22:53:09 -0300

    ad commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -14,3 +14,52 @@ export const fetchPOST = async (route, body, aditionalConfigs) => {
     const data = await res.json()
     return data
 }
+
+export const fetchPUT = async (route, body, aditionalConfigs) => {
+    const baseConfig = {
+        method: 'PUT',
+        mode: "cors",
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            "Authorization": `Bearer ${localStorage.getItem("token")}`
+        },
+        body: JSON.stringify(body)
+    }
+    const config = aditionalConfigs ? {...baseConfig, aditionalConfigs} : baseConfig
+    const res = await fetch(route, config)
+    const data = await res.json()
+    return data
+}
+
+export const fetchDELETE = async (route, aditionalConfigs) => {
+    const baseConfig = {
+        method: 'DELETE',
+        mode: "cors",
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            "Authorization": `Bearer ${localStorage.getItem("token")}`
+        },
+    }
+    const config = aditionalConfigs ? {...baseConfig, aditionalConfigs} : baseConfig
+    const res = await fetch(route, config)
+    const data = await res.json()
+    return data
+}
+
+export const fetchGET = async (route, aditionalConfigs) => {
+    const baseConfig = {
+        method: 'GET',
+        mode: "cors",
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            "Authorization": `Bearer ${localStorage.getItem("token")}`
+        },
+    }
+    const config = aditionalConfigs ? {...baseConfig, aditionalConfigs} : baseConfig
+    const res = await fetch(route, config)
+    const data = await res.json()
+    return data
+}
